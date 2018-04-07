ec1b408f88e56f1c4835164f3ab281bf5897943c
Thibaud Beaufils <thibaud.beaufils@gmail.com>
2018-04-07 16:25:39 +0200

    Creation du git

---
+++ Mercure/Mercure/Integration.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Mercure
+{
+    public partial class Integration : Form
+    {
+        private bool crush; //enable overwrite
+
+        public Integration()
+        {
+            InitializeComponent();
+            this.label2.ForeColor = Color.Red;
+            this.label2.Text = " ";
+            this.radioButton2.Checked = true;
+            this.progressBar1.ForeColor = Color.Plum;
+            this.progressBar1.Maximum = 10;
+            this.progressBar1.Step = 1;
+            this.crush = true;
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            if (this.textBox1.Text.CompareTo("") == 0)
+                this.label2.Text = "Veuillez sélectionner un fichier !!";
+            else
+            {
+                db_management dbmanage = new db_management();
+                if (radioButton2.Checked)
+                    dbmanage.flushTable();
+                dbmanage.integration(this.textBox1.Text);
+            }
+        }
+
+        //Boutton de recherche de fichier
+        private void button2_Click(object sender, EventArgs e)
+        {
+            String path;
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Title = "Sélectionner le fichier xml";
+            ofd.Filter = "Fichiers XML (*.XML)|*.XML";
+            ofd.Multiselect = false;
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                path = ofd.FileName;
+                this.textBox1.Text = path;
+                this.label2.Text = " ";
+            }
+            else
+            {
+                this.label2.Text = "Veuillez sélectionner un fichier valide !!";
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Mercure/Mercure/Main.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace Mercure
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.fichierToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.importerDesDonnéesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fichierToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(284, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Location = new System.Drawing.Point(0, 239);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(284, 22);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // fichierToolStripMenuItem
+            // 
+            this.fichierToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.importerDesDonnéesToolStripMenuItem});
+            this.fichierToolStripMenuItem.Name = "fichierToolStripMenuItem";
+            this.fichierToolStripMenuItem.Size = new System.Drawing.Size(54, 20);
+            this.fichierToolStripMenuItem.Text = "Fichier";
+            // 
+            // importerDesDonnéesToolStripMenuItem
+            // 
+            this.importerDesDonnéesToolStripMenuItem.Name = "importerDesDonnéesToolStripMenuItem";
+            this.importerDesDonnéesToolStripMenuItem.Size = new System.Drawing.Size(189, 22);
+            this.importerDesDonnéesToolStripMenuItem.Text = "Importer des données";
+            this.importerDesDonnéesToolStripMenuItem.Click += new System.EventHandler(this.importerDesDonnéesToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fichierToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem importerDesDonnéesToolStripMenuItem;
+    }
+}
+
